4e103f01319011d4cbaafc06a490194b81b89907
Stewart Pavitt <stewart.pavitt@gmail.com>
2021-05-11 09:00:59 +0100

    Added latest version of identityServer

---
+++ 1/IdentityServerAspNetIdentity/Config.cs
@@ -0,0 +1,109 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer.MyExtensions;
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServerAspNetIdentity
+{
+    public static class Config
+    {
+
+        public static IdentityResource MyRugbyIdentityResource()
+        {
+            var claims = new List<string>()
+            {
+                "position",
+                "club",
+                "clountry"
+            };
+            return new IdentityResource("rugby", claims);
+        }
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                SPIdentityResources.Rugby(),
+            };
+
+
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("api1", "My API")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "mvc",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                     RedirectUris = { "https://localhost:5002/signin-oidc" },
+                     AllowOfflineAccess = true,
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "rugby",
+                        "api1"
+                    }
+                },
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                },
+                new Client
+                {
+                    ClientId = "js",
+                    ClientName = "JavaScript Client",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
+
+                    RedirectUris =           { "https://localhost:5003/callback.html" },
+                    PostLogoutRedirectUris = { "https://localhost:5003/index.html" },
+                    AllowedCorsOrigins =     { "https://localhost:5003" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api1",
+                        "rugby"
+                    }
+                }
+            };
+    }
+}
---
+++ 1/IdentityServerAspNetIdentity/SeedData.cs
@@ -0,0 +1,203 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System;
+using System.Linq;
+using System.Security.Claims;
+using IdentityModel;
+using IdentityServerAspNetIdentity.Data;
+using IdentityServerAspNetIdentity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace IdentityServerAspNetIdentity
+{
+    public class SeedData
+    {
+
+       
+        public static void EnsureSeedData(string connectionString)
+        {
+            var services = new ServiceCollection();
+            services.AddLogging();
+            services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlite(connectionString));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            using (var serviceProvider = services.BuildServiceProvider())
+            {
+                using (var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+                {
+                    var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+                    context.Database.Migrate();
+
+                    var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+                    bool recreateUsers = false;
+
+                    ManageAlice(userMgr, recreateUsers);
+                    ManageBob(userMgr, recreateUsers);
+                    ManageCarson(userMgr, recreateUsers);
+                }
+            }
+        }
+
+        private static void ManageCarson(UserManager<ApplicationUser> userMgr, bool delete = false)
+        {
+            var carson = userMgr.FindByNameAsync("carson").Result;
+
+            if (delete)
+            {
+                _ = userMgr.DeleteAsync(carson).Result;
+                carson = null;
+            }
+
+            if (carson == null)
+            {
+                AddCarson(carson, userMgr);
+            }
+            else
+            {
+                Log.Debug("carson already exists");
+            }
+        }
+
+
+        private static void ManageBob(UserManager<ApplicationUser> userMgr, bool delete = false)
+        {
+            var bob = userMgr.FindByNameAsync("bob").Result;
+
+            if (delete)
+            {
+                _ = userMgr.DeleteAsync(bob).Result;
+                bob = null;
+            }
+
+            if (bob == null)
+            {
+                AddBob(bob, userMgr);
+            }
+            else
+            {
+                Log.Debug("bob already exists");
+            }
+        }
+
+        private static void ManageAlice(UserManager<ApplicationUser> userMgr, bool delete = false)
+        {
+            var alice = userMgr.FindByNameAsync("alice").Result;
+
+            if (delete)
+            {
+                _ = userMgr.DeleteAsync(alice).Result;
+                alice = null;
+            }
+
+            if (alice == null)
+            {
+                AddAlice(alice, userMgr);
+            }
+            else
+            {
+                Log.Debug("alice already exists");
+            }
+        }
+
+        private static void AddAlice(ApplicationUser alice, UserManager<ApplicationUser> userMgr)
+        {
+            alice = new ApplicationUser
+            {
+                UserName = "alice",
+                Email = "AliceSmith@email.com",
+                EmailConfirmed = true,
+            };
+            var result = userMgr.CreateAsync(alice, "Sitekit123").Result;
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.First().Description);
+            }
+
+            result = userMgr.AddClaimsAsync(alice, new Claim[]{
+                            new Claim(JwtClaimTypes.Name, "Alice Smith"),
+                            new Claim(JwtClaimTypes.GivenName, "Alice"),
+                            new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                            new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
+                            new Claim("position", "lock"),
+                            new Claim("club", "Exeter"),
+                            new Claim("country", "Scotland")
+                        }).Result;
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.First().Description);
+            }
+            Log.Debug("alice created");
+        }
+
+        private static void AddBob(ApplicationUser bob, UserManager<ApplicationUser> userMgr)
+        {
+            bob = new ApplicationUser
+            {
+                UserName = "bob",
+                Email = "BobSmith@email.com",
+                EmailConfirmed = true
+            };
+            var result = userMgr.CreateAsync(bob, "Sitekit123").Result;
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.First().Description);
+            }
+
+            result = userMgr.AddClaimsAsync(bob, new Claim[]{
+                            new Claim(JwtClaimTypes.Name, "Bob Smith"),
+                            new Claim(JwtClaimTypes.GivenName, "Bob"),
+                            new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                            new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
+                            new Claim("position", "prop"),
+                            new Claim("club", "Edinburgh"),
+                            new Claim("country", "England")
+                        }).Result;
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.First().Description);
+            }
+            Log.Debug("bob created");
+        }
+
+        private static void AddCarson(ApplicationUser carson, UserManager<ApplicationUser> userMgr)
+        {
+            carson = new ApplicationUser
+            {
+                UserName = "carson",
+                Email = "Carson@email.com",
+                EmailConfirmed = true
+            };
+            var result = userMgr.CreateAsync(carson, "Sitekit123").Result;
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.First().Description);
+            }
+
+            result = userMgr.AddClaimsAsync(carson, new Claim[]{
+                            new Claim(JwtClaimTypes.Name, "Carson Wells"),
+                            new Claim(JwtClaimTypes.GivenName, "carson"),
+                            new Claim(JwtClaimTypes.FamilyName, "Wells"),
+                            new Claim(JwtClaimTypes.WebSite, "http://carson.com"),
+                            new Claim("position", "Flanker"),
+                            new Claim("club", "Leinster"),
+                            new Claim("country", "Ireland")
+                        }).Result;
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.First().Description);
+            }
+            Log.Debug("carson created");
+        }
+
+    }
+}
