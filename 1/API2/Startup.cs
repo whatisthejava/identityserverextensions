1affb6ff6abc1c52baa2cc0dd312b5e344390477
Stewart Pavitt <stewart.pavitt@gmail.com>
2021-05-11 09:44:04 +0100

    API 2 now connecting with same clientid/secret/scope

--- 1/API2/Startup.cs
+++ 1/API2/Startup.cs
@@ -30,7 +30,7 @@ namespace API2
                 options.AddPolicy("ApiScope", policy =>
                 {
                     policy.RequireAuthenticatedUser();
-                    policy.RequireClaim("scope", "api2");
+                    policy.RequireClaim("scope", "api1");
                 });
             });
 
